7f4356608da9bb5104183e11e27aa56c453a80ef
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2024-12-23 18:17:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-# Specify the provider
-provider "aws" {
-  region = "us-east-1" # Set your preferred region
-}
-
 # Create a VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
