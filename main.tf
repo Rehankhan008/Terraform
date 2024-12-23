61dffb68344a2a3f8c02ef5d71c1379f98c24a80
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2024-12-23 18:16:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,57 @@
+# Specify the provider
+provider "aws" {
+  region = "us-east-1" # Set your preferred region
+}
+
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "tf-vpc"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "tf-igw"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a" # Specify the availability zone
+  tags = {
+    Name = "tf-subnet"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "main_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "tf-rt"
+  }
+}
+
+# Add a Route to the Internet Gateway in the Route Table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.main_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
+
+# Associate the Subnet with the Route Table
+resource "aws_route_table_association" "main_rta" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_rt.id
+}
+
+
 
